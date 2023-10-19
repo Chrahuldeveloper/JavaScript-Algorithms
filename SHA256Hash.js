e512b23197f74501e338ae28d13d8d692f01fa11
Rahul <119114775+Chrahuldeveloper@users.noreply.github.com>
2023-10-19 10:14:34 +0530

    Delete SHA256Hash.js

--- SHA256Hash.js
+++
@@ -1,8 +0,0 @@
-const crypto = require("crypto");
-function SHA256Hash(input) {
-  const hash = crypto.createHash("sha256");
-  hash.update(input);
-  return hash.digest("hex");
-}
-
-console.log(SHA256Hash("Rahul"));
